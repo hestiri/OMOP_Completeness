b4bd655ab85caefcf5966fa5de08dd077132c822
Ábtin Estiri <estiri.hossein@gmail.com>
2015-12-17 15:23:08 -0800

    This is the script for the analysis of completeness

--- OMOP_COMP_Test.R
+++ OMOP_COMP_Test.R
@@ -3,6 +3,7 @@ install.packages("data.table");require(data.table)
 # set the working directory
 setwd("~/OneDrive UW/OneDriveBusiness/omop/UW_Evaluation_2015-01-14")
 
+
 for (i in 1:length(dir())) assign(dir()[i], read.table(dir()[i], header = T, sep="|",quote = "",fill = TRUE))
 
  # ##READ OMOP TABLES with read.table (slower)
